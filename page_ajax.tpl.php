bf6fa650a38f1256a4b832fd0e2eb370578a91cb
wunluv <santhan@mojahmedia.net>
2025-07-03 23:53:26 +0200

    Refactor: Clean up page_ajax.tpl.php and move CSS to style.css

--- page_ajax.tpl.php
+++ page_ajax.tpl.php
@@ -1,27 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language ?>" xml:lang="<?php print $language ?>">
 <head>
-<style>
-.normal {
-clear:both;
-padding:5px 0;
-}
-legend {
-color:#B79715;
-font-size:1.2em;
-}
-.normal select,
-.normal label,
-.normal input {
-float:left;
-font-family:georgia,arial;
-font-size:1.2em;
-width:130px !important;
-}
-.normal input,
-.normal select {
-width:auto;
-margin:0 0 0 5px;
-}
-</style>
-<style type="text/css" media="all">@import "/heaven/sites/all/themes/h/style.css";</style>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
 </head>
-<?php print  $content;  ?>
+<body>
+  <?php print $content; ?>
+</body>
+</html>
